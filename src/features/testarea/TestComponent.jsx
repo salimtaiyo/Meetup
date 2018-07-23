ffe7dd40e1705ee02e96999dbc65ef4f46a41d0e
salimtaiyo <ichigeki.ccCsc@hotmail.com>
2018-07-22 23:56:49 -0400

    redux boilerplate

--- src/features/testarea/TestComponent.jsx
+++ src/features/testarea/TestComponent.jsx
@@ -1,18 +1,25 @@
 import React from 'react'
 import { connect } from 'react-redux';
+import {incrementCounter, decrementCounter} from './testAction';
+import { Button } from 'semantic-ui-react';
 
 const mapToState = (state) => ({
     data: state.test.data
 })
+
+const action = { incrementCounter, decrementCounter }
 
 const TestComponent = (props) => {
   return (
     <div>
       <h1> shalom dahal </h1>
       <h2> {props.data}</h2>
+      <Button onClick={props.incrementCounter} content="Increment" color='green'/>
+      <Button onClick={props.decrementCounter} color='red' content="Decrement"/>
+
     </div>
   )
 }
 
-export default connect(mapToState)(TestComponent);
+export default connect(mapToState, action )(TestComponent);
 
